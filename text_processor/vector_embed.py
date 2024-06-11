d23a02df474b5b993e6a4ddb6d66a791528479e6
Gowri <gowriks12@gmail.com>
2024-06-10 22:09:00 -0400

    changing Embedder

--- text_processor/vector_embed.py
+++ text_processor/vector_embed.py
@@ -3,8 +3,11 @@ from langchain_community.vectorstores import FAISS
 
 
 def get_vector_store(text_chunks):
+    # embeddings = HuggingFaceEmbeddings(
+    #     model_name="sentence-transformers/all-MiniLM-L6-v2"
+    # )
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="BAAI/bge-small-en-v1.5"
     )
 
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
